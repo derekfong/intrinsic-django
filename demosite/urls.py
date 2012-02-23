a57b5055be634397170c2959d30922cbe1882a4c
Kevin Mann <kma50@sfu.ca>
2012-02-23 14:43:31 -0800

    trying again

--- demosite/urls.py
+++ demosite/urls.py
@@ -7,9 +7,9 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
 	url(r'^$', direct_to_template, {'template': 'head.html'}, name="index"),
-	url(r'^polls/', include('polls.urls')),
+	url(r'^polls/', include('demoapp.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-	url(r'^accounts/', include('polls.urls')),
+	url(r'^accounts/', include('demoapp.urls')),
 )
 
