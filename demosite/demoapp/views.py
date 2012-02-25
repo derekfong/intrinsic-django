6bc545577e5ae0cdedee57b9a46c6633669bda3d
Kevin Mann <kma50@sfu.ca>
2012-02-24 19:07:41 -0800

    Fix Views

--- demosite/demoapp/views.py
+++ demosite/demoapp/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response, get_object_or_404
-from demoapp.models import Choice, Poll
+from demoapp.models import Choice, Poll, Voted
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
